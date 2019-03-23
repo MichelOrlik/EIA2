b442c6b01bc4b3fecf172f794e62e093043074ab
MichelOrlik <44029019+MichelOrlik@users.noreply.github.com>
2019-03-23 13:54:47 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Gedankengang der Funktion als Kommentar */
+/* Bei Aufgabe 0 muss, laut den Geboten, noch kein Kommentar hier her. */
+function helloworld() {
+    var person = prompt("Enter name for free Shrek 2 DVD", "Your name"); /* So wird ein prompt erstellt, wie ich aus w3schools erfahren habe */
+    var hola = person + "! Was geht ab?"; /* Erstelle eine neue Variabel die den Satz enthält */
+    console.log(hola); /* Aus Aufgabe: Noch auf der Konsole anzeigen */
+    document.getElementById("Text").innerHTML = hola; /* Erstelle im HTML Dokument den davor definierten Satz, welcher als Variabel "hola" erstellt wurde */
+}
+//# sourceMappingURL=main.js.map

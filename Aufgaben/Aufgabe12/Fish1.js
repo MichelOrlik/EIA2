cd14e8ab986ec70852dc7f00908730d20e7b1e74
MichelOrlik <44029019+MichelOrlik@users.noreply.github.com>
2019-06-30 10:56:27 +0200

    Update Fish1.js

--- Aufgaben/Aufgabe12/Fish1.js
+++ Aufgaben/Aufgabe12/Fish1.js
@@ -43,13 +43,13 @@ var Task12;
             if (this.x + 100 < 0) {
                 this.x = 1330;
             }
-            if (this.y > 750) {
+            if (this.y > 170) {
                 this.y = 0;
             }
             if (this.x > 1330) {
                 this.x = -50;
             }
-            if (this.y + 5 < 0) {
+            if (this.y + 2 < 0) {
                 this.y = 750;
             }
         }

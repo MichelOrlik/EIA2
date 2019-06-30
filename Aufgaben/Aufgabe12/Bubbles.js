154af71fe587f236ced63853c1607ca37a23e02a
MichelOrlik <44029019+MichelOrlik@users.noreply.github.com>
2019-06-30 10:58:57 +0200

    Update Bubbles.js

--- Aufgaben/Aufgabe12/Bubbles.js
+++ Aufgaben/Aufgabe12/Bubbles.js
@@ -6,16 +6,16 @@ var Task12;
             this.x = Math.random() * Task12.crc.canvas.width;
             this.y = Math.random() * Task12.crc.canvas.height;
             this.dx = 0;
-            this.dy = Math.random() * 4 - 2;
-            this.r = Math.random() * (30 - 5) + 5;
+            this.dy = Math.random() * 2 - 1;
+            this.r = Math.random() * 30;
         }
         draw() {
-            let bubles = new Path2D();
-            bubles.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
-            Task12.crc.fillStyle = "#26E9FF";
-            Task12.crc.strokeStyle = "E0FFFA";
-            Task12.crc.fill(bubles);
-            Task12.crc.stroke(bubles);
+            let bubbles = new Path2D();
+            bubbles.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+            Task12.crc.fillStyle = "#7FFFD4";
+            Task12.crc.strokeStyle = "#6495ED";
+            Task12.crc.fill(bubbles);
+            Task12.crc.stroke(bubbles);
         }
         move() {
             this.dy = -Math.abs(this.dy);
